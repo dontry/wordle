c787d1a50abfb4a68d51082f4e46a7a2497a0708
Dong Cai <dongc@blackmagicdesign.com>
2022-01-24 22:37:19 +1100

    feat(modal): create modals for not in list warning and result

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import GameContext from "./context/GameContext";
 import Board from "./components/Board";
+import Keyboard from './components/Keyboard';
+import Dialog from './components/Dialog';
+import classNames from 'classnames';
 
 import "./App.css";
-import Keyboard from './components/Keyboard';
 
 function App() {
-  const { gameStatus: result } = useContext(GameContext);
+  const { gameStatus: result, answer } = useContext(GameContext);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const dialogClasses = classNames(
+    "text-xl text-center font-semibold",
+    {
+      'text-green-500': result === 'won',
+      'text-red-500': result === 'lost',
+    }
+  )
+
+  useEffect(() => {
+    if (result === 'won' || result === 'lost') {
+      setDialogOpen(true)
+    }
+  }, [result]);
+
+  const handleClose = useCallback(() => {
+    setDialogOpen(false)
+  }, [])
+
 
   return (
     <div className="App max-w-screen-sm mx-auto grid">
@@ -18,13 +39,15 @@ function App() {
         <section className="flex flex-col items-center justify-center">
           <Board />
         </section>
-        <section className="mt-4">
-          {result === "won" && <h2 className="text-center text-2xl text-green-500">You won!</h2>}
-          {result === "lost" && <h2 className="text-center text-2xl text-red-500">You lost!</h2>}
-        </section>
         <section>
           <Keyboard />
         </section>
+        <Dialog open={dialogOpen} onClose={handleClose}>
+          <div className="grid justify-center items-center">
+            <h1 className={dialogClasses}>{`You ${result}!`}</h1>
+            {result === 'lost' &&  <p className='mt-3 text-center'>The answer is {answer}.</p>}
+          </div>
+        </Dialog>
       </main>
     </div>
   );
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useCallback, useContext } from 'react';
 import GameContext from '../context/GameContext';
 import { KeyType } from '../types';
 import './keyboard.css';
@@ -6,49 +6,59 @@ import Key from './Key';
 import { checkFilled, checkInWordList, handleKeyPress } from '../lib/utils';
 
 const keyMap: KeyType[][] = [
-	['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-	['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-	['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '⌫']
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '⌫']
 ]
 
 const Keyboard = () => {
-	const { keyStatuses, guesses, curRowIndex, dispatch } = useContext(GameContext);
-	const keyRowClasses = "keyboard-row grid grid-flow-col gap-2 justify-content-stretch";
+  const { keyStatuses, guesses, curRowIndex, dispatch } = useContext(GameContext);
+  const keyRowClasses = "keyboard-row grid grid-flow-col gap-2 justify-content-stretch";
 
-	const handleClick = (key: string) => {
-		key = key.toLowerCase();
-		handleKeyPress(
-			key as KeyType,
-			guesses,
-			curRowIndex,
-			dispatch,
-			() => {
-				if (
-					checkFilled(guesses[curRowIndex]) &&
-					!checkInWordList(guesses[curRowIndex])
-				) {
-					alert('Not in the word list');
-					dispatch({ type: 'PAUSE' });
-					setTimeout(() => {
-						dispatch({ type: 'RESUME' });
-					}, 500);
-				} else {
-					dispatch({ type: "ENTER" });
-				}
-			}
-		)
-	}
+  const handleKeyClick = useCallback((key: string) => {
+    key = key.toLowerCase();
+    handleKeyPress(
+      key as KeyType,
+      guesses,
+      curRowIndex,
+      dispatch,
+    )
+  }, [dispatch]);
 
-	return (<div className="keyboard grid gap-2 p-3">
-		{keyMap.map((row, i) => {
-			return <div className={keyRowClasses} key={i}>
-				{row.map((key, j) => {
-					const status = keyStatuses[key];
-					return <Key key={key} status={status} char={key} onClick={handleClick} />
-				})}
-			</div>
-		})}
-	</div>)
+  const handleEnterKeyClick = useCallback((key: string) => {
+    key = key.toLowerCase();
+    handleKeyPress(
+      key as KeyType,
+      guesses,
+      curRowIndex,
+      dispatch,
+      () => {
+        if (
+          checkFilled(guesses[curRowIndex]) &&
+          !checkInWordList(guesses[curRowIndex])
+        ) {
+          dispatch({ type: 'PAUSE' });
+          setTimeout(() => {
+            dispatch({ type: 'RESUME' });
+          }, 500);
+        } else {
+          dispatch({ type: "ENTER" });
+        }
+      }
+    )
+  }, [curRowIndex, guesses, dispatch]);
+
+  return (<div className="keyboard grid gap-2 p-3">
+    {keyMap.map((row, i) => {
+      return <div className={keyRowClasses} key={i}>
+        {row.map((key, j) => {
+          const status = keyStatuses[key] || 'empty';
+          const handleClick = key === 'enter' ? handleEnterKeyClick : handleKeyClick;
+          return <Key key={key} status={status} char={key} onClick={handleClick} />
+        })}
+      </div>
+    })}
+  </div>)
 }
 
 export default Keyboard;
